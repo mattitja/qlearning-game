28444c29dbb417ef0b419aeab086192815e36874
Matthias Miro <matthias.miro@hbt.de>
2025-04-18 12:01:13 +0200

    bigger grid, better ASCII art

--- src/game.py
+++ src/game.py
@@ -1,50 +1,68 @@
 import random
 import os
 
+bloc = '#'
+star = 'O'
+play = '@'
+spac = ' '
+goal = 'X'
+walx = '|'
+waly = '='
+
+walls = [bloc, walx, waly]
+
 grid = [
-    ['A', ' ', ' ', ' ', ' ', ' '],
-    [' ', 'o', 'o', ' ', 'o', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' '],
-    ['o', ' ', ' ', 'o', 'o', 'o'],
-    [' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', 'o', ' ', ' ', 'Z'],
+    [waly, waly, waly, waly, waly, waly, waly, waly, waly, waly, waly, waly, waly, waly],
+    [walx, star, bloc, spac, bloc, bloc, bloc, bloc, bloc, spac, spac, spac, bloc, walx],
+    [walx, spac, bloc, spac, spac, spac, spac, spac, spac, spac, bloc, spac, spac, walx],
+    [walx, spac, bloc, spac, bloc, bloc, bloc, bloc, bloc, bloc, bloc, bloc, spac, walx],
+    [walx, spac, bloc, spac, bloc, bloc, bloc, bloc, bloc, bloc, bloc, bloc, spac, walx],
+    [walx, spac, bloc, spac, spac, spac, spac, spac, spac, spac, spac, bloc, spac, walx],
+    [walx, spac, bloc, bloc, bloc, bloc, bloc, bloc, bloc, bloc, spac, bloc, spac, walx],
+    [walx, spac, spac, spac, spac, spac, spac, spac, spac, spac, spac, bloc, goal, walx],
+    [waly, waly, waly, waly, waly, waly, waly, waly, waly, waly, waly, waly, waly, waly],
 ]
 
 Q = {}
 
-GRID_SIZE = len(grid)
+GRID_SIZE_Y = len(grid)
+GRID_SIZE_X = len(grid[0])
 
-ALL_ACTIONS = ['L', 'R', 'U', 'D']
+left = '←'
+right = '→'
+top = '↑'
+down = '↓'
 
-for x in range(GRID_SIZE):
-    for y in range(GRID_SIZE):
+ALL_ACTIONS = [left, right, top, down]
+
+for x in range(GRID_SIZE_X):
+    for y in range(GRID_SIZE_Y):
         Q[(x, y)] = {action: 0 for action in ALL_ACTIONS}
 
 def move (current_position, desired_action):
     x, y = current_position
-    if (desired_action == 'L') and x > 0:
+    if (desired_action == left) and x > 0:
         x -= 1
-    elif (desired_action == 'R') and x < GRID_SIZE - 1:
+    elif (desired_action == right) and x < GRID_SIZE_X - 1:
         x += 1
-    elif (desired_action == 'U') and y > 0:
+    elif (desired_action == top) and y > 0:
         y -= 1
-    elif (desired_action == 'D') and y < GRID_SIZE - 1:
+    elif (desired_action == down) and y < GRID_SIZE_Y - 1:
         y += 1
     return (x, y)
 
 def get_reward(current_position):
     x, y = current_position
-    if grid[y][x] == 'A':
-        return -1
-    elif grid[y][x] == 'Z':
+    if grid[y][x] == star:
+        return -100
+    elif grid[y][x] == goal:
         return 100
-    elif grid[y][x] == 'o':
+    elif grid[y][x] in walls:
         return -10
     else:
         return -0.1
 
 def get_allowed_actions(current_position):
-    x, y = current_position
     allowed_actions = []
     for action in ALL_ACTIONS:
         next_position = move(current_position, action)
@@ -65,11 +83,11 @@ def choose_action(current_position, q, epsilon):
         return get_best_action(q, allowed_actions, current_position)
 
 def print_policy():
-    for y in range(GRID_SIZE):
+    for y in range(GRID_SIZE_Y):
         row = ''
-        for x in range(GRID_SIZE):
+        for x in range(GRID_SIZE_X):
             current_position = (x, y)
-            if grid[y][x] in ('A', 'Z', 'o'):
+            if grid[y][x] in (star, goal, *walls):
                 letter = grid[y][x]
             else:
                 letter = get_best_action(Q, get_allowed_actions(current_position), current_position)
@@ -78,11 +96,11 @@ def print_policy():
 
 def print_grid_with_current_pos(current_pos):
     current_x, current_y = current_pos
-    for y in range(GRID_SIZE):
+    for y in range(GRID_SIZE_Y):
         row = ''
-        for x in range(GRID_SIZE):
+        for x in range(GRID_SIZE_X):
             if x == current_x and y == current_y:
-                letter = 'O'
+                letter = play
             else:
                 letter = grid[y][x]
             row += " " + letter + " "
@@ -92,9 +110,9 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 # Q-Learning Algorithmus
-def q_learning(episodes=15000, alpha=0.2, gamma=0.9, epsilon=0.2):
+def q_learning(episodes=1500, alpha=0.2, gamma=0.9, epsilon=0.1):
     for e in range(episodes):
-        current_pos = (0, 0)
+        current_pos = (1, 1)
         done = False
         step = 0
         while not done:
@@ -116,10 +134,13 @@ def q_learning(episodes=15000, alpha=0.2, gamma=0.9, epsilon=0.2):
 
             step += 1
             #sleep(0.001)
-            if grid[new_pos[1]][new_pos[0]] in ('A', 'Z', 'o'):
+            if grid[new_pos[1]][new_pos[0]] in (star, goal, *walls):
                 done = True
 
 if __name__ == "__main__":
-    q_learning()
-    clear_screen()
-    print_policy()
+    try:
+        q_learning()
+        clear_screen()
+        print_policy()
+    except KeyboardInterrupt:
+        print_policy()
--- tests/test_game.py
+++ tests/test_game.py
@@ -9,22 +9,22 @@ M = 0 #min
 G = GRID_SIZE-1 #grid_six max
 
 def test_move():
-    assert move((M, M), 'D') == (M, M+1)
-    assert move((M, M), 'U') == (M, M)
-    assert move((M, M), 'L') == (M, M)
-    assert move((M, M), 'R') == (M+1, M)
-    assert move((G, G), 'D') == (G, G)
-    assert move((G, G), 'U') == (G, G-1)
-    assert move((G, G), 'L') == (G-1, G)
-    assert move((G, G), 'R') == (G, G)
-    assert move((M, G), 'D') == (M, G)
-    assert move((M, G), 'U') == (M, G-1)
-    assert move((M, G), 'L') == (M, G)
-    assert move((M, G), 'R') == (M+1, G)
-    assert move((G, M), 'D') == (G, M+1)
-    assert move((G, M), 'U') == (G, M)
-    assert move((G, M), 'L') == (G-1, M)
-    assert move((G, M), 'R') == (G, M)
+    assert move((M, M), '↓') == (M, M+1)
+    assert move((M, M), '↑') == (M, M)
+    assert move((M, M), '←') == (M, M)
+    assert move((M, M), '→') == (M+1, M)
+    assert move((G, G), '↓') == (G, G)
+    assert move((G, G), '↑') == (G, G-1)
+    assert move((G, G), '←') == (G-1, G)
+    assert move((G, G), '→') == (G, G)
+    assert move((M, G), '↓') == (M, G)
+    assert move((M, G), '↑') == (M, G-1)
+    assert move((M, G), '←') == (M, G)
+    assert move((M, G), '→') == (M+1, G)
+    assert move((G, M), '↓') == (G, M+1)
+    assert move((G, M), '↑') == (G, M)
+    assert move((G, M), '←') == (G-1, M)
+    assert move((G, M), '→') == (G, M)
 
 def test_get_reward():
     assert get_reward((M,M)) == -1.0
@@ -32,33 +32,33 @@ def test_get_reward():
     assert get_reward((1,1)) == -0.1
 
 def test_get_allowed_actions():
-    assert get_allowed_actions((M, M)) == ['R', 'D']
+    assert get_allowed_actions((M, M)) == ['→', '↓']
     assert get_allowed_actions((M + 1, M + 1)) == ALL_ACTIONS
 
 def test_get_best_action():
     current_position = (0, 0)
-    q = {current_position: {'L': 1, 'R': 2, 'U': 3, 'D': 4}}
-    assert get_best_action(q, ['L'], current_position) == 'L'
-    assert get_best_action(q, ['L', 'D'], current_position) == 'D'
+    q = {current_position: {'←': 1, '→': 2, '↑': 3, '↓': 4}}
+    assert get_best_action(q, ['←'], current_position) == '←'
+    assert get_best_action(q, ['←', '↓'], current_position) == '↓'
 
 def test_get_best_action_but_there_are_more(monkeypatch):
     current_position = (0, 0)
-    q = {current_position: {'L': 1, 'R': 1, 'U': 1, 'D': 1}}
-    assert get_best_action(q, ['L'], current_position) == 'L'
+    q = {current_position: {'←': 1, '→': 1, '↑': 1, '↓': 1}}
+    assert get_best_action(q, ['←'], current_position) == '←'
     monkeypatch.setattr('random.choice', lambda p: p[0])
-    assert get_best_action(q, ['L', 'D'], current_position) == 'L'
+    assert get_best_action(q, ['←', '↓'], current_position) == '←'
 
 def test_choose_action_random(monkeypatch):
     current_position = (0, 0)
-    q = {current_position: {'L': 1, 'R': 2, 'U': 3, 'D': 4}}
-    monkeypatch.setattr('src.game.get_allowed_actions', lambda p: ['L'])
+    q = {current_position: {'←': 1, '→': 2, '↑': 3, '↓': 4}}
+    monkeypatch.setattr('src.game.get_allowed_actions', lambda p: ['←'])
     monkeypatch.setattr('random.random', lambda: 0.00)
-    assert choose_action(current_position, q) == 'L'
+    assert choose_action(current_position, q) == '←'
 
 def test_choose_action_best(monkeypatch):
     current_position = (0, 0)
-    q = {current_position: {'L': 1, 'R': 2, 'U': 3, 'D': 4}}
-    monkeypatch.setattr('src.game.get_allowed_actions', lambda p: ['L', 'R'])
-    monkeypatch.setattr('src.game.get_best_action', lambda x, y, z: 'R')
+    q = {current_position: {'←': 1, '→': 2, '↑': 3, '↓': 4}}
+    monkeypatch.setattr('src.game.get_allowed_actions', lambda p: ['←', '→'])
+    monkeypatch.setattr('src.game.get_best_action', lambda x, y, z: '→')
     monkeypatch.setattr('random.random', lambda: 1)
-    assert choose_action(current_position, q, 0.1) == 'R'
+    assert choose_action(current_position, q, 0.1) == '→'
